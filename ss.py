e99848d9d088ea60966d343da7e4c020c82b7078
Lee <limeng89@foxmail.com>
2017-05-20 14:34:35 +0800

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import requests
+import re
+import json
+from subprocess import Popen
+
+URL = "http://ss.ishadowx.com/"
+
+file = open('gui-config.json', 'r')
+
+j = json.load(file)
+file.close()
+configs = j['configs']
+
+print('读取来自%s的SS服务器数据...' % URL, end='')
+r = requests.get(URL)
+r.encoding = 'utf-8'
+
+data = re.findall('IP Address:.+?>(\S+)<.+?Port：(\d+)<.+?Password:.+?(\d+)<.+?Method:(\S+)<', r.text, re.S)
+print('成功！\n')
+configs.clear()
+for i in range(len(data)):
+    dict = {'server': 'a.jpip.pro', 'server_port': 443, 'password': '83969268', 'method': ' \
+    aes-256-cfb', 'remarks': '', 'auth': False, 'timeout': 5}
+    e = data[i]  
+    dict['server'] = e[0]
+    dict['server_port'] = e[1]
+    dict['password'] = e[2]
+    dict['method'] = e[3]
+    configs.append(dict)
+
+print('总共获取:%d\n' % (i + 1))
+
+#print (j)
+
+
+file = open('gui-config.json', 'w')
+json.dump(j, file)
+file.close()
+os.chdir('./')
+print (os.getcwd())
+os.system('taskkill /F /IM shadowsocks.exe')    
+Popen('shadowsocks.exe')
